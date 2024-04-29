7480816382c28b1e47574715cfbb45c9bd32b530
Adegboyega Ademola <a.adegboyega11@gmail.com>
2024-04-29 21:11:28 +0100

    AirBnB_clone_v3

--- models/base_model.py
+++ models/base_model.py
@@ -7,19 +7,28 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        """Instantiate a new model"""
+        from models import storage
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        storage.new(self)
+        
+        if kwargs:
+            if 'created_at' in kwargs:
+                created_at_arg = kwargs.pop('created_at')
+                if isinstance(created_at_arg, str):
+                    self.created_at = datetime.strptime(created_at_arg, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.created_at = created_at_arg
+            if 'updated_at' in kwargs:
+                updated_at_arg = kwargs.pop('updated_at')
+                if isinstance(updated_at_arg, str):
+                    self.updated_at = datetime.strptime(updated_at_arg, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.updated_at = updated_at_arg
+            if '__class__' in kwargs:
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
